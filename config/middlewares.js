80743f46fc90a5965e86ede9bacd10e4dd7736d5
Pouria Jahandideh <pjsofts@gmail.com>
2024-09-17 16:36:13 +0330

    Added cors headers

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://frontecademy.com'],
+      headers: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
